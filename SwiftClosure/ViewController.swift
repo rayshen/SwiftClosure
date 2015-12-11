cede87260d9b8e1fc80f5eeed24f0d4ec9472823
rayshen <rayshen1012@gmail.com>
2015-12-11 09:45:14 +0800

    Initial Commit

---
+++ SwiftClosure/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftClosure
+//
+//  Created by shen on 15/12/11.
+//  Copyright © 2015年 shen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
